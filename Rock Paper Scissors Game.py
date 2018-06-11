37a1c4fff85e5f268bffb00cb4d3e3660b8cf9fa
Omar Palmer-Lopez <37731320+OPL94@users.noreply.github.com>
2018-06-11 21:07:49 +0100

    Rock, Paper, Scissors
    This change is to incorporate the rock, paper, scissors game in Python programming.

---
+++ Rock Paper Scissors Game.py
@@ -0,0 +1,46 @@
+#This program is to make a game rock, paper, scissor!
+
+from random import randint
+
+player1 = input("Pick rock, paper, or scissors!")
+
+if player1 == "rock" or player1 == "paper" or player1 == "scissors":
+    print("Player chose " + player1 + ".")
+else:
+    print("Please pick rock, paper or scissors!")
+
+#This will make the computer randomly pick a number associated with rock paper and scissors!    
+computer = randint(1,3)
+if computer == 1:
+    computer = "rock"
+elif computer == 2:
+    computer = "paper"
+else:
+    computer = "scissors"
+
+print("Computer chose " + computer + ".")
+
+#Now to determine who wins! Player or Computer!
+
+if player1 == "rock" and computer == "rock":
+    print("Draw!")
+elif player1 == "rock" and computer == "paper":
+    print("Computer wins!")
+elif player1 == "rock" and computer == "scissors":
+    print("Player wins!")
+elif player1 == "paper" and computer == "rock":
+    print("Player wins!")
+elif player1 == "paper" and computer == "paper":
+    print("Draw!")
+elif player1 == "paper" and computer == "scissors":
+    print("Computer wins!")
+elif player1 == "scissors" and computer == "rock":
+    print("Computer wins!")
+elif player1 == "scissors" and computer == "paper":
+    print("Player wins!")
+elif player1 == "scissors" and computer == "scissors":
+    print("Draw!")
+
+print("Press the play button to reset the game!")
+
+
